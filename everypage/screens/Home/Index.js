3d7b9d42137d06e894b11c8c1cc2bbc2738fef1a
psoni01 <cmungcharoenpo00@mylangara.ca>
2022-10-07 16:39:57 -0700

    add: result rendering area w/ dummie compos

--- everypage/screens/Home/Index.js
+++ everypage/screens/Home/Index.js
@@ -1,14 +1,37 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, Box } from "native-base";
 import BottomTab from "../Assets/BottomTab";
 import Search from "../Assets/Search";
+import MyLibraryCard from "../Assets/Cards/MyLibraryCard";
+import { ScrollView } from "react-native";
 
 const Home = ({ navigation }) => {
   return (
     <VStack>
       <Search navigation={navigation} />
+      <Text mx={2} my={2}>
+        All (9)
+      </Text>
+      <ScrollView>
+        <Box
+          px={2}
+          py={3}
+          w="100%"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="space-evenly"
+        >
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+          <MyLibraryCard navigation={navigation} />
+        </Box>
+      </ScrollView>
     </VStack>
   );
 };
-
 
 export default Home;
