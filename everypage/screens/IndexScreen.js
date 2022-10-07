c783cfd84e9859c6ba0f2b0bb4c47651067bb484
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2022-10-07 15:11:34 -0700

    Changes done to change the title of Screen

--- everypage/screens/IndexScreen.js
+++ everypage/screens/IndexScreen.js
@@ -1,10 +1,22 @@
 import { AntDesign } from "@expo/vector-icons";
 import { Fab, Icon, Text, VStack } from "native-base";
+import { useEffect } from "react";
 import { Button, StyleSheet } from "react-native";
 import BottomTab from "./Assets/BottomTab";
 import Home from "./Home/Index";
 
 const IndexScreen = ({ navigation }) => {
+  useEffect(() => {
+    //over here right now I am doing as per wireframes
+    // but when we get user from backend we will have
+    // user name setup  for the user.
+    //=============================
+    navigation.setOptions({
+      title: "Mita's Library",
+      //  value === "" ? "No title" : value,
+    });
+    // =============================
+  }, [navigation]);
   return (
     <VStack style={Style.mainContainerStyle}>
       <Home style={{ flex: 1 }} navigation={navigation} />
