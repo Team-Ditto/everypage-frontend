4bcfef7981de942d7ceab65e0f5c5deb4e24398a
123-Teju <59676568+123-Teju@users.noreply.github.com>
2022-10-20 15:02:41 -0700

    Worked on Signup location page

--- everypage/screens/User/Profile.js
+++ everypage/screens/User/Profile.js
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import SearchSortFilter from "./SearchSortFilter";
+
 export default function Profile() {
   return (
     <View>
       <Text>Profile</Text>
+      {/* <Location /> */}
+      <SearchSortFilter />
     </View>
   );
 }
