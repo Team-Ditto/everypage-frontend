eadebaf2c4d6c685e23c37c2e6fb9c432bf1ea77
gabrielgimenes92 <gabrielcgimenes@gmail.com>
2022-10-20 00:29:57 -0700

    feat: Adds ForLater and Requested pages

---
+++ screens/Wishlist/ForLater.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "native-base"
+
+export default function ForLater({ navigation }) {
+  
+    return (
+        <Text>
+            For Later page
+        </Text>
+    )
+};
---
+++ screens/Wishlist/Requested.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "native-base"
+
+export default function Requested({ navigation }) {
+  
+    return (
+        <Text>
+            Requested page
+        </Text>
+    )
+};
