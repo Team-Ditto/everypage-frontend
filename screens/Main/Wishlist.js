2448b7536725433e0ec452d7444e70e6eab256c1
gabrielgimenes92 <gabrielcgimenes@gmail.com>
2022-10-20 00:29:57 -0700

    feat: Adds ForLater and Requested pages

--- screens/Main/Wishlist.js
+++ screens/Main/Wishlist.js
@@ -2,28 +2,39 @@ import { VStack, Text, Center, Box, HStack, Button } from "native-base";
 import React from "react";
 import { useState } from "react";
 import Search from "../Assets/Search";
+import ForLater from "../Wishlist/ForLater";
+import Requested from "../Wishlist/Requested";
 
 import { LibraryData } from "../../constants/LibraryData";
 
 export default function Wishlist({ navigation }) {
   const [libData, setLibData] = useState(LibraryData);
+  const [isForLater, setIsForLater] = useState(true);
+
+  //setCurScreen("forLater")
+
+  function handleInput(v) {
+    setIsForLater(v);
+  }
 
   return (
     <VStack>
       <Search navigation={navigation} />
 
       <HStack>
-        <Button>
+        <Button onPress={() => handleInput(true)}>
           For Later
         </Button>
-        <Button>
+        <Button onPress={() => handleInput(false)}>
           Requested
         </Button>
       </HStack>
-
-      { /* curScreen="forLater" ? <ForLater /> : <Requested /> */ } 
-
       <Text>Wishlist</Text>
+      <VStack>
+        <Box>
+          { isForLater ? <ForLater /> : <Requested /> } 
+        </Box>
+      </VStack>
     </VStack>
   );
 
