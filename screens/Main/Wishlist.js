c8e0fd66a3f3bba215035b94dd69c48be7d449ae
gabrielgimenes92 <gabrielcgimenes@gmail.com>
2022-10-21 01:48:17 -0700

    feat: working top 'For Later' and 'Requested' buttons

--- screens/Main/Wishlist.js
+++ screens/Main/Wishlist.js
@@ -1,5 +1,6 @@
 import { VStack, Text, Center, Box, HStack, Button } from "native-base";
 import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import Search from "../Assets/Search";
 import ForLater from "../Wishlist/ForLater";
 import Requested from "../Wishlist/Requested";
@@ -18,14 +19,24 @@ export default function Wishlist({ navigation }) {
     <VStack>
       <Search navigation={navigation} />
       <Box backgroundColor="#FDF5EA" mx="4%" mt="18px" borderRadius="10px" borderColor="#DC924D" borderStyle="solid" borderWidth="1px">
-      <HStack display="flex" flexDirection="row">
-        <Button backgroundColor="#DC924D" flexGrow={true} borderRadius="10px" onPress={() => handleInput(true)}>
-          For Later
-        </Button>
-        <Button backgroundColor="#FDF5EA" _text={{ color: "#DC924D"}} flexGrow={true} borderRadius="10px" onPress={() => handleInput(false)}>
-          Requested
-        </Button>
-      </HStack>
+      { isForLater ? 
+        <HStack display="flex" flexDirection="row">
+          <Button backgroundColor="#DC924D" flexGrow={true} borderRadius="10px" onPress={() => handleInput(true)}>
+            For Later
+          </Button>
+          <Button backgroundColor="#FDF5EA" _text={{ color: "#DC924D"}} flexGrow={true} borderRadius="10px" onPress={() => handleInput(false)}>
+            Requested
+          </Button>
+        </HStack>
+        :
+        <HStack display="flex" flexDirection="row">
+          <Button backgroundColor="#FDF5EA" _text={{ color: "#DC924D"}} flexGrow={true} borderRadius="10px" onPress={() => handleInput(true)}>
+            For Later
+          </Button>
+          <Button backgroundColor="#DC924D" flexGrow={true} borderRadius="10px" onPress={() => handleInput(false)}>
+            Requested
+          </Button>
+        </HStack>}
       </Box>
       <VStack>
         <Box>
@@ -35,3 +46,17 @@ export default function Wishlist({ navigation }) {
     </VStack>
   );
 }
+
+const Styles = StyleSheet.create({
+  buttonActive: {
+    backgroundColor: "#DC924D",
+    flexGrow: true,
+    borderRadius: "10px",
+  },
+  buttonInactive: {
+    backgroundColor: "#FDF5EA",
+    color: "#DC924D",
+    flexGrow: true,
+    borderRadius: "10px",
+  }
+})
