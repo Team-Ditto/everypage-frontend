f1c4dd0781d1f04469bbeadc5a92757db33a3cdb
psoni01 <cmungcharoenpo00@mylangara.ca>
2022-10-24 10:13:40 -0700

    remove: genreTab add: semiclickable bookStatusTab

--- screens/Main/Index.js
+++ screens/Main/Index.js
@@ -1,47 +1,38 @@
-import { VStack, Text, Box, Button } from "native-base";
-import Search from "../Assets/Search";
-import { ScrollView } from "react-native";
-import { useState } from "react";
+import { VStack, Text, Box, Button } from 'native-base';
+import Search from '../Assets/Search';
+import { ScrollView } from 'react-native';
+import { useState } from 'react';
 
-import { LibraryData } from "../../constants/LibraryData";
-import MyLibraryCard from "../Cards/Library/MyLibraryCard";
-import FloatingButtons from "../Assets/FloatingButtons";
+import { LibraryData } from '../../constants/LibraryData';
+import { BOOK_STATUS } from '../../constants/index';
+import MyLibraryCard from '../Cards/Library/MyLibraryCard';
+import FloatingButtons from '../Assets/FloatingButtons';
 
 const Home = ({ navigation }) => {
   const [libData, setLibData] = useState(LibraryData);
-
-  const genreData = [
-    "Art",
-    "Crime",
-    "Fiction",
-    "Biology",
-    "Art",
-    "Crime",
-    "Fiction",
-    "Biology"
-  ];
+  const [bookStatus, setBookStatus] = useState('All');
 
   return (
     <VStack>
       {/* Search component */}
       <Search navigation={navigation} />
       {/* button slider */}
-      <Text m={2}>Genre</Text>
       <ScrollView
-        style={{ display: "flex", flexDirection: "row", margin: 5 }}
+        style={{ display: 'flex', flexDirection: 'row', margin: 5 }}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
       >
-        {genreData.map((genre, idx) => {
+        {BOOK_STATUS.map((status, idx) => {
           return (
-            <Box mx={1} key={idx} h={60}>
+            <Box mx={1} key={idx} h='55px' width='120px'>
               <Button
                 px={5}
-                variant={"solid"}
-                color={"muted.800"}
-                borderRadius="sm"
+                variant={'solid'}
+                color={'muted.800'}
+                borderRadius='md'
+                onPress={e => setBookStatus(status)}
               >
-                {genre}
+                {status}
               </Button>
             </Box>
           );
@@ -50,21 +41,12 @@ const Home = ({ navigation }) => {
 
       {/* My Library Data Collection */}
       <Text mx={2} my={2}>
-        All ({libData.length})
+        {bookStatus} {libData.length}
       </Text>
       <ScrollView>
-        <Box
-          py={3}
-          px={2}
-          w="100%"
-          flexDirection="row"
-          flexWrap="wrap"
-          justifyContent="space-between"
-        >
+        <Box py={3} px={2} w='100%' flexDirection='row' flexWrap='wrap' justifyContent='space-between'>
           {libData.map((data, id) => {
-            return (
-              <MyLibraryCard key={id} data={data} navigation={navigation} />
-            );
+            return <MyLibraryCard key={id} data={data} navigation={navigation} />;
           })}
         </Box>
       </ScrollView>
