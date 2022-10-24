df73793a6a7e17a21ad0001f7a2b777930d4ac07
psoni01 <cmungcharoenpo00@mylangara.ca>
2022-10-24 10:17:17 -0700

    edit: import genreData add: navigation to Genres

--- screens/Main/Discover.js
+++ screens/Main/Discover.js
@@ -1,31 +1,12 @@
-import React, { useState } from "react";
-import Search from "../Assets/Search";
-import {
-  Box,
-  Text,
-  Button,
-  ScrollView,
-  VStack,
-  HStack,
-  Icon,
-} from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
-import { LibraryData } from "../../constants/LibraryData";
-import MyLibraryCard from "../Cards/Library/MyLibraryCard";
+import React, { useState } from 'react';
+import Search from '../Assets/Search';
+import { Box, Text, Button, ScrollView, VStack, HStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { LibraryData, genreData } from '../../constants/LibraryData';
+import MyLibraryCard from '../Cards/Library/MyLibraryCard';
 
 export default function Discover({ navigation }) {
   const [similarBookData, setSimilarBookData] = useState(LibraryData);
-
-  const genreData = [
-    "Art",
-    "Crime",
-    "Fiction",
-    "Biology",
-    "Art",
-    "Crime",
-    "Fiction",
-    "Biology",
-  ];
 
   return (
     <VStack>
@@ -33,34 +14,30 @@ export default function Discover({ navigation }) {
       <Search navigation={navigation} />
 
       {/* Genre Generation */}
-      <HStack style={{ display: "flex", justifyContent: "space-between" }}>
-        <Text m={2} fontSize="md">
+      <HStack style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Text m={2} fontSize='md'>
           Genre
         </Text>
         <Button
-          variant="unstyled"
-          endIcon={
-            <Icon as={MaterialIcons} name="keyboard-arrow-right" size="md" />
-          }
+          variant='unstyled'
+          endIcon={<Icon as={MaterialIcons} name='keyboard-arrow-right' size='md' />}
+          onPress={() => {
+            navigation.navigate('Genres');
+          }}
         >
           View All
         </Button>
       </HStack>
 
       <ScrollView
-        style={{ display: "flex", flexDirection: "row", margin: 5 }}
+        style={{ display: 'flex', flexDirection: 'row', margin: 5 }}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
       >
         {genreData.map((genre, idx) => {
           return (
             <Box mx={1} key={idx} h={60}>
-              <Button
-                px={5}
-                variant={"solid"}
-                color={"muted.800"}
-                borderRadius="sm"
-              >
+              <Button px={5} variant={'solid'} color={'muted.800'} borderRadius='sm'>
                 {genre}
               </Button>
             </Box>
@@ -70,28 +47,14 @@ export default function Discover({ navigation }) {
 
       {/* View below the Genre Tab */}
 
-      <Text m={2} fontWeight="bold" fontSize="2xl">
+      <Text m={2} fontWeight='bold' fontSize='2xl'>
         Books you might like
       </Text>
 
       <ScrollView>
-        <Box
-          py={3}
-          px={2}
-          w="100%"
-          flexDirection="row"
-          flexWrap="wrap"
-          justifyContent="space-between"
-        >
+        <Box py={3} px={2} w='100%' flexDirection='row' flexWrap='wrap' justifyContent='space-between'>
           {similarBookData.map((data, id) => {
-            return (
-              <MyLibraryCard
-                key={id}
-                data={data}
-                navigation={navigation}
-                showWishListIcon={true}
-              />
-            );
+            return <MyLibraryCard key={id} data={data} navigation={navigation} showWishListIcon={true} />;
           })}
         </Box>
       </ScrollView>
