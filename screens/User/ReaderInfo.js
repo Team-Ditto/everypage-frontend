e7805f48757743816e0d0d387bee7c7c8c56d00b
Shubham Verma <49226983+SVerma799@users.noreply.github.com>
2022-10-27 21:58:14 -0700

    Changes done to show ReaderInfo with style

--- screens/User/ReaderInfo.js
+++ screens/User/ReaderInfo.js
@@ -1,7 +1,7 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React, { useEffect } from 'react';
 import { Button, VStack } from 'native-base';
-import { BlueShades } from '../../assets/style/color';
+import { BlueShades, OrangeShades } from '../../assets/style/color';
 export default function ReaderInfo({ navigation }) {
   useEffect(() => {
     navigation.setOptions({
@@ -10,11 +10,29 @@ export default function ReaderInfo({ navigation }) {
   }, []);
   return (
     <>
-      <VStack>
-        <Text>You're almost there @username</Text>
-        <Text>What type of reader are you?</Text>
+      <VStack mt={10} mb={5} mx={5}>
+        <Text style={{ fontSize: '34' }}>You're almost there @username</Text>
+        <Text style={{ marginTop: 20, fontSize: '24' }}>What type of reader are you?</Text>
       </VStack>
-      <VStack></VStack>
+      <VStack mx={5} space={30}>
+        <VStack style={styles.readerTypeContainer} space={1}>
+          <Text style={{ fontSize: '24' }}>Fast Reader</Text>
+          <Text style={{ fontSize: '16' }}>Read one or more than one book in a day</Text>
+        </VStack>
+        <VStack style={styles.readerTypeContainer}>
+          <Text style={{ fontSize: '24' }}>Casual Reader</Text>
+          <Text style={{ fontSize: '16' }}>Read book as often as I like</Text>
+        </VStack>
+        <VStack
+          style={styles.readerTypeContainer}
+          onPress={() => {
+            alert('asd');
+          }}
+        >
+          <Text style={{ fontSize: '24' }}>Slow Reader</Text>
+          <Text style={{ fontSize: '16' }}>Read one book in more than 3 days</Text>
+        </VStack>
+      </VStack>
       <VStack
         style={{
           display: 'flex',
@@ -25,7 +43,10 @@ export default function ReaderInfo({ navigation }) {
         }}
       >
         <Button
-          width='80%'
+          width='90%'
+          size='lg'
+          mt={10}
+          borderRadius={10}
           bg={BlueShades.primaryBlue}
           onPress={() => {
             navigation.navigate('BottomTab');
@@ -37,3 +58,14 @@ export default function ReaderInfo({ navigation }) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  readerTypeContainer: {
+    backgroundColor: OrangeShades.quaternaryOrange,
+    borderWidth: 1,
+    borderColor: OrangeShades.primaryOrange,
+    paddingVertical: 20,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+  },
+});
