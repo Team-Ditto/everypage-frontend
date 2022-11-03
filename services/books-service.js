5fffc08c3699f80ef0a84764c9dc6835d2a1e036
OctoPol7 <thechumpower@gmail.com>
2022-11-03 00:17:11 -0700

    add: fetch book data from b/end

--- services/books-service.js
+++ services/books-service.js
@@ -1,3 +1,11 @@
 import axiosRequest from './api';
 
 // see users-service.js for example
+
+export async function getAllBooks() {
+  try {
+    return await axiosRequest.get(`books/all`);
+  } catch (err) {
+    console.log(err);
+  }
+}
