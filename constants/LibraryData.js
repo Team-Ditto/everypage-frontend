7c0ad79bfa53ab3fefc55e2fa5d8215cbb84c7ad
OctoPol7 <thechumpower@gmail.com>
2022-11-05 00:29:34 -0700

    add: genre icons for genre tab on Discover

--- constants/LibraryData.js
+++ constants/LibraryData.js
@@ -1,3 +1,12 @@
+import ActionAndAdventure from '../assets/genre-icons/action-and-adventure.png';
+import Classics from '../assets/genre-icons/classics.png';
+import Comics from '../assets/genre-icons/comics.png';
+import CrimeAndMystery from '../assets/genre-icons/crime-and-mystery.png';
+import Fantasy from '../assets/genre-icons/fantasy.png';
+import HistoricalFiction from '../assets/genre-icons/historical-fiction.png';
+import Horror from '../assets/genre-icons/horror.png';
+import LiteraryFiction from '../assets/genre-icons/literary-fiction.png';
+
 export const LibraryData = [
   {
     images: ['https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1474154022i/3.jpg'],
@@ -56,7 +65,7 @@ export const genreData = [
   'Horror',
   'Literary Fiction',
   'Memoir',
-  'Non Fiction',
+  'Non-Fiction',
   'Poetry',
   'Romance',
   'Science Fiction',
@@ -65,4 +74,39 @@ export const genreData = [
   'Suspense & Thriller',
   'True Crime',
   "Women's Fiction",
+];
+
+export const genreDiscover = [
+  {
+    genre: 'Action & Adventure',
+    icon: ActionAndAdventure,
+  },
+  {
+    genre: 'Classics',
+    icon: Classics,
+  },
+  {
+    genre: 'Comics',
+    icon: Comics,
+  },
+  {
+    genre: 'Crime & Mystery',
+    icon: CrimeAndMystery,
+  },
+  {
+    genre: 'Fantasy',
+    icon: Fantasy,
+  },
+  {
+    genre: 'Historical Fiction',
+    icon: HistoricalFiction,
+  },
+  {
+    genre: 'Horror',
+    icon: Horror,
+  },
+  {
+    genre: 'Literary Fiction',
+    icon: LiteraryFiction,
+  },
 ];
