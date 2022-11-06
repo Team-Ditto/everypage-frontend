44cd6f1477974fb54cf6565214b8cd5280d51ba3
gabrielgimenes92 <gabrielcgimenes@gmail.com>
2022-11-06 14:15:08 -0800

    creates Notification dummy data and progress on notification card component

--- screens/Notifications/NotificationsMain.js
+++ screens/Notifications/NotificationsMain.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Text } from 'native-base';
+import React, { useState } from 'react';
+import { Divider, Text, VStack } from 'native-base';
+import NotificationCard from '../Cards/Notifications/NotificationCard';
+import { NotificationData } from '../../constants/NotificationData';
 
-const NotificationsMain = () => {
-  return <Text>NotificationsMain</Text>;
+const NotificationsMain = ({ navigation }) => {
+  const [notifications, setNotifications] = useState(NotificationData);
+
+  return (
+    <VStack>
+      {notifications?.map((data, id) => {
+        return (
+          <>
+            <NotificationCard key={id} data={data} navigation={navigation} />
+            <Divider />
+          </>
+        );
+      })}
+    </VStack>
+  );
 };
 
 export default NotificationsMain;
