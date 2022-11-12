052a07360033ec7d150be5e1de9b488d3ac11493
OctoPol7 <thechumpower@gmail.com>
2022-11-11 23:38:07 -0800

    style: bottom tab active status

--- stacks/TabStack.js
+++ stacks/TabStack.js
@@ -8,12 +8,17 @@ import Community from '../screens/Main/Community';
 import Notifications from '../screens/Main/Notifications';
 
 import MyLibraryIcon from '../assets/navigation-icons/library.png';
+import MyLibraryIconActive from '../assets/navigation-icons/library_active.png';
 import WishlistIcon from '../assets/navigation-icons/wishlist.png';
+import WishlistIconActive from '../assets/navigation-icons/wishlist_active.png';
 import DiscoverIcon from '../assets/navigation-icons/discover.png';
+import DiscoverIconActive from '../assets/navigation-icons/discover_active.png';
 import CommunityIcon from '../assets/navigation-icons/community.png';
+import CommunityIconActive from '../assets/navigation-icons/community_active.png';
 import ProfileIcon from '../assets/navigation-icons/profile.png';
+import ProfileIconActive from '../assets/navigation-icons/profile_active.png';
 
-import { BlueShades } from '../assets/style/color';
+import { BlueShades, OrangeShades } from '../assets/style/color';
 import Map from '../screens/Assets/Map';
 export default function TabStack() {
   const TabNavigator = createBottomTabNavigator();
@@ -22,42 +27,44 @@ export default function TabStack() {
       padding: 5,
       height: 90,
     },
+    tabBarActiveTintColor: OrangeShades.primaryOrange,
   };
+
   return (
     <TabNavigator.Navigator screenOptions={screenOptions}>
       <TabNavigator.Screen
         name='Home'
         component={IndexScreen}
         options={({ route }) => ({
-          tabBarIcon: ({ color, size }) => <Image source={MyLibraryIcon} color={BlueShades.primaryBlue} size={34} />,
+          tabBarIcon: ({ focused }) => <Image source={focused ? MyLibraryIconActive : MyLibraryIcon} size={34} />,
         })}
       />
       <TabNavigator.Screen
         name='Wishlist'
         component={Wishlist}
         options={({ route }) => ({
-          tabBarIcon: ({ color, size }) => <Image source={WishlistIcon} color={BlueShades.primaryBlue} size={34} />,
+          tabBarIcon: ({ focused }) => <Image source={focused ? WishlistIconActive : WishlistIcon} size={34} />,
         })}
       />
       <TabNavigator.Screen
         name='Discover'
         component={Discover}
         options={({ route }) => ({
-          tabBarIcon: ({ color, size }) => <Image source={DiscoverIcon} color={BlueShades.primaryBlue} size={34} />,
+          tabBarIcon: ({ focused }) => <Image source={focused ? DiscoverIconActive : DiscoverIcon} size={34} />,
         })}
       />
       <TabNavigator.Screen
         name='Community'
         component={Community}
         options={({ route }) => ({
-          tabBarIcon: ({ color, size }) => <Image source={CommunityIcon} color={BlueShades.primaryBlue} size={24} />,
+          tabBarIcon: ({ focused }) => <Image source={focused ? CommunityIconActive : CommunityIcon} size={24} />,
         })}
       />
       <TabNavigator.Screen
         name='Profile'
         component={Profile}
         options={({ route }) => ({
-          tabBarIcon: ({ color, size }) => <Image source={ProfileIcon} color={BlueShades.primaryBlue} size={34} />,
+          tabBarIcon: ({ focused }) => <Image source={focused ? ProfileIconActive : ProfileIcon} size={34} />,
         })}
       />
     </TabNavigator.Navigator>
