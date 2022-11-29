5a40d07706220d90121a2bea1b275d9619b3019c
Ashim Karki <aceem123@gmail.com>
2022-11-28 16:03:34 -0800

    fixed the chat reference for books

--- screens/Chats/Messages.js
+++ screens/Chats/Messages.js
@@ -1,15 +1,16 @@
 import { useContext, useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { Image, Text, View, HStack, ScrollView } from 'native-base';
+import { Image, Text, View, HStack, ScrollView, Box } from 'native-base';
 import { doc, onSnapshot } from 'firebase/firestore';
 
 import { ChatContext } from '../../contexts/ChatContext';
 import { db } from '../../firebase';
 import Message from './Message';
-import { BlueShades, OrangeShades } from '../../assets/style/color';
+import { BlueShades, OrangeShades, SuccessColor, InUseColor, OnHoldColor } from '../../assets/style/color';
 
 const Messages = () => {
   const [banner, setBanner] = useState(null);
+  const [latestBook, setLatestBook] = useState();
   const [messages, setMessages] = useState([]);
   const { data } = useContext(ChatContext);
 
@@ -17,6 +18,7 @@ const Messages = () => {
     const unsubscribe = onSnapshot(doc(db, 'chats', data.chatId), doc => {
       if (doc.exists()) {
         setBanner(doc.data().banner);
+        setLatestBook(doc.data().book);
         setMessages(doc.data().messages);
       }
     });
@@ -26,6 +28,40 @@ const Messages = () => {
     };
   }, [data.chatId]);
 
+  const statusStyle = [
+    {
+      status: 'Available',
+      backgroundColor: SuccessColor.successBG,
+      textColor: SuccessColor.successText,
+    },
+    {
+      status: 'In-Use',
+      backgroundColor: InUseColor.inUseBG,
+      textColor: InUseColor.inUseText,
+    },
+    {
+      status: 'On-Hold',
+      backgroundColor: OnHoldColor.onHoldBG,
+      textColor: OnHoldColor.onHoldText,
+    },
+  ];
+
+  getStyling = () => {
+    switch (latestBook.borrowingStatus) {
+      case 'Available':
+        return statusStyle[0];
+
+      case 'In-Use':
+        return statusStyle[1];
+
+      case 'On-Hold':
+        return statusStyle[2];
+
+      default:
+        console.log('Book status not available');
+    }
+  };
+
   return (
     <View style={styles.messages}>
       <HStack justifyContent={'center'} alignItems={'center'} marginY={3}>
@@ -33,8 +69,44 @@ const Messages = () => {
         <Text style={styles.banner}>{banner}</Text>
       </HStack>
       <ScrollView>
-        {/* insert the book card here. See if we can make use of existing card components, if not do not 
-        create a new component and just do the markup here. For data refer to firebase/chats collection */}
+        {!!latestBook && (
+          <Box>
+            <HStack display='flex' flex='1' style={styles.book}>
+              <Box style={{ display: 'flex', flex: 1 }}>
+                <Image
+                  style={{ position: 'absolute', left: 0, width: '100%', height: '100%' }}
+                  borderRadius='10px'
+                  source={{
+                    uri: latestBook.images[0],
+                  }}
+                  alt={latestBook.title}
+                />
+              </Box>
+              <Box w='55%' ml='3%' display='flex'>
+                <Text flex='1' fontWeight='semibold' fontSize='md' numberOfLines={2}>
+                  {latestBook.title}
+                </Text>
+                <Text fontSize='md' numberOfLines={2}>{latestBook.author}</Text>
+                <Box
+                  bgColor={getStyling().backgroundColor}
+                  borderRadius='4px'
+                  borderColor={getStyling().textColor}
+                  borderStyle='solid'
+                  borderWidth='1px'
+                  p='4px'
+                  marginY='10px'
+                  marginRight='auto'
+                >
+                  <Text color={getStyling().textColor}>{latestBook.borrowingStatus}</Text>
+                </Box>
+                <Text>Owned by</Text>
+                <Text fontSize='md' underline color={OrangeShades.primaryOrange}>
+                  {latestBook.ownerName}
+                </Text>
+              </Box>
+            </HStack>
+          </Box>
+        )}
         {messages.map(m => (
           <Message message={m} key={m._id} />
         ))}
@@ -42,7 +114,15 @@ const Messages = () => {
     </View>
   );
 };
+
 const styles = StyleSheet.create({
+  book: {
+    backgroundColor: BlueShades.tertiaryBlue,
+    padding: 20,
+    borderRadius: 15,
+    overflow: 'hidden',
+    marginBottom: 20,
+  },
   messages: {
     backgroundColor: '#fff',
     height: '100%',
